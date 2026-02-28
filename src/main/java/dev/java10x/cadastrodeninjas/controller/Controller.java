69199d8ba0143455edb958cb38e22d2db3779209
Sara Santos <contatosarams@gmail.com>
2026-02-27 22:22:03 -0300

    adiciona a classe Controller ao package controller

--- src/main/java/dev/java10x/cadastrodeninjas/controller/Controller.java
+++ src/main/java/dev/java10x/cadastrodeninjas/controller/Controller.java
@@ -1,4 +1,4 @@
-package dev.java10x.cadastrodeninjas;
+package dev.java10x.cadastrodeninjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
