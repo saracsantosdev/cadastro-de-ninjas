6bc51f8df4bfdfd2f9dcb7aa3b9a17e25c4cdc43
Sara Santos <contatosarams@gmail.com>
2026-02-27 21:02:57 -0300

    first commit

---
+++ src/main/java/dev/java10x/cadastrodeninjas/CadastrodeninjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeninjasApplication.class, args);
+	}
+
+}
